835c1735216ccc30e7708768b7cfc251d7c114df
Jaskirat <jaskirat055@gmail.com>
2026-01-27 00:27:04 +0530

    Add: Docker Setup

--- scripts/init-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Create the comfyautomate database
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE comfyautomate;
-    GRANT ALL PRIVILEGES ON DATABASE comfyautomate TO temporal;
-EOSQL
